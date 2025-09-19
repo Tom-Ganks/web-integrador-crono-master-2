5c137e890698a6b9c7f76146d1abd75e8404c4ae
Tom-ganks <kingnaofumi007@gmail.com>
2025-09-18 22:31:33 -0300

    commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 // src/services/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://xrvlludmkeytytiymzlu.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydmxsdWRta2V5dHl0aXltemx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NTk3MDAsImV4cCI6MjA2NDEzNTcwMH0.ws26ZPPy_cKSA21ZDmtV06rZJf8ogwxhATuNSWSSiX0';
 
 let supabaseClient;
 
